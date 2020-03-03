c760f43f4f0466d9bb19ebb3d564f486cfd88468
Nicholas Buzzerio <nick.buzzerio@gmail.com>
2020-03-03 16:53:10 -0500

    migrated db from local to cloud

--- dbHelpers/db.js
+++ dbHelpers/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/sagittarius', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://nbuzzer:77mongoDB77@fec-availability-mdczx.mongodb.net/sagittarius', {useNewUrlParser: true});
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
